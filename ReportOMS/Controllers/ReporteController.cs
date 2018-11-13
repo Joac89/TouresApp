c9d59f27639f703e48d1592b833a955ce9920034
Jesus Aguilar <joac.aguilar@hotmail.com>
2018-11-13 00:50:17 -0500

    Ejemplo reporte
    Ejemplo de un nuevo proyecto con MVC y EF para generar los reportes y verlos desde el OMS por medio de AHREF

---
+++ ReportOMS/Controllers/ReporteController.cs
@@ -0,0 +1,32 @@
+﻿using ReportOMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReportOMS.Controllers
+{
+    public class ReporteController : Controller
+    {
+        // GET: Reporte
+        public ActionResult ReporteIndex()
+        {           
+            return View();
+        }
+
+        public ActionResult ReporteData(ReporteModel data)
+        {
+            var result = new List<OMS_PRODUCTO>();
+            using (var context = new OMSEntitiesSQL())
+            {
+                result = context.OMS_PRODUCTO.Where(
+                    x => x.FECHA_SALIDA >= data.FechaSalida &&
+                    x.FECHA_LLEGADA <= data.FechaLlegada
+                    ).ToList();
+            }
+
+            return View("ReporteData", result);
+        }
+    }
+}
---
+++ ReportOMS/Models/ReporteModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReportOMS.Models
+{
+    public class ReporteModel
+    {
+        public DateTime FechaSalida { get; set; }
+        public DateTime FechaLlegada { get; set; }
+    }
+}
