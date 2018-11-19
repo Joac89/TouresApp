a2d0a82cbed553d378407cb4349e336c03ea49b2
Juna Ricardo Gonzalez <jrgonzalezsepulveda@gmail.com>
2018-11-19 15:54:16 -0500

    depuracion
    depuracion

--- TouresOMS/ClientApp/app/components/customer/customerP.component.ts
+++ TouresOMS/ClientApp/app/components/customer/customerP.component.ts
@@ -53,41 +53,6 @@ export class CustomerPComponent {
     update: boolean = false;
     authorize: boolean = false;
 
-    citys: City[] = [
-        { id: 1, name: 'Bogota' },
-        { id: 2, name: 'Medellin' },
-        { id: 3, name: 'Barranquilla' },
-        { id: 4, name: 'Cali' },
-        { id: 5, name: 'Madrid' },
-        { id: 6, name: 'Nueva York' },
-        { id: 7, name: 'Paris' },
-        { id: 11027, name: 'Berlin' }
-    ];
-
-    country: Country[] = [
-        { id: 'Colombia', name: 'Colombia' },
-        { id: 'España', name: 'España' },
-        { id: 'USA', name: 'USA' },
-        { id: 'Francia', name: 'Francia' },
-        { id: 'Alemania', name: 'Alemania' }
-    ];
-
-    typespect: TypeSpect[] = [
-        { id: 1, name: 'Nacional' },
-        { id: 14, name: 'Internacional' },        
-    ];
-
-    typetrans: TypeTrans[] = [
-        { id: 1, name: 'Aereo' },
-        { id: 14, name: 'Terrestre' },
-    ];
-
-    typehospedaje: TypeHospedaje[] = [
-        { id: 1, name: 'Nacional' },
-        { id: 14, name: 'Internacional' },
-    ];
-
-
     constructor(private datePipe: DatePipe, @Inject('BASE_URL') baseUrl: string, private route: ActivatedRoute, private router: Router, private http: Http, private storeService: StoreService, private loaderService: LoaderService, private commonService: CommonService) {
         this.path = baseUrl;
 
@@ -96,7 +61,7 @@ export class CustomerPComponent {
             this.typeSearch = params['type'];
             this.search.data = [];
 
-            if (this.textSearch) this.getSearch();
+            //if (this.textSearch) this.getSearch();
         });
 
     }
@@ -105,42 +70,7 @@ export class CustomerPComponent {
         return this.datePipe.transform(new Date(date), 'yyyy-MM-dd');
     }
 
-    getSearch(inner: boolean = false) {
-        this.loaderService.start();
-        this.pag += 1;
 
-        if (inner && this.pag > 1) {
-            this.textSearch = btoa(this.textSearch);
-        } else {
-            this.pag = 1;
-            this.endPage = false;
-        }
-
-        this.http.get(this.path + "api/product/search/" + this.textSearch + "/" + this.typeSearch + "/" + this.pag).map(response => response.json()).subscribe(result => {
-            this.aux = result;
-            this.product = result.data[0];
-
-            this.product.fechaEspectaculo = this.transformDate(this.product.fechaEspectaculo);
-            this.product.fechaSalida = this.transformDate(this.product.fechaSalida);
-            this.product.fechaLlegada = this.transformDate(this.product.fechaLlegada);
-
-            console.log(this.product);
-                       
-            if (this.aux.data.length == 0 || this.aux.data.length < 4) this.endPage = true;
-                        
-            for (var i = 0; i < this.aux.data.length; ++i) {
-                this.search.data.push(this.aux.data[i]);
-            }
-                        
-            this.loaderService.end();
-
-        }, error => {
-            this.loaderService.end();
-            console.error(error);
-        });
-
-        this.textSearch = atob(this.textSearch);
-    }
 
     getDetail(item: any) {
         this.current = item;
